b638925f52dbcfd1d8a14030021e88e860a6bde5
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2022-06-30 12:46:14 +0430

    Add Error Code To Library
    Error From Server Check And Added To if.

--- library.php
+++ library.php
@@ -3,6 +3,8 @@ function tag_code($input){
     $get=file_get_contents('https://tag-code.ir/encode/?get='.$input);
     if($get){
         $r=$get;
+    }elseif($get=='Error'){
+        $r='Error,Please Try Again!';
     }else{
         $r='Error,Cannot Connect To Server,Please Check Your Internet Connection!';
     }
